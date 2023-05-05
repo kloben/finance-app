5d16f1424b48246e9899bfd791c828de07ca8a55
DavidValero <dvdvalero89@gmail.com>
2023-05-05 13:08:22 +0200

    Add custom checkbox

---
+++ src/components/ui/forms/VgInputCheck.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+
+defineProps<{
+  label: string
+  modelValue?: boolean
+}>()
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void
+}>()
+</script>
+
+<template>
+  <div class="vg-input">
+    <div class="text-body-2">{{ label }}</div>
+    <div class="check-container"
+         :class="{active: modelValue}"
+         @click="emit('update:modelValue', !modelValue)"
+    >
+      <div class="handle"></div>
+      <div class="spacer"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "src/styles/colors";
+$animation: 0.1s;
+
+.vg-input {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+}
+.text-body-2 {
+  user-select: none;
+  flex: 1;
+}
+
+.check-container {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  width: 70px;
+  height: 40px;
+  border-radius: 30px;
+  background: $secondary;
+  padding: 2px;
+  cursor: pointer;
+  transition: background $animation linear;
+
+  &.active {
+    background: $primary;
+
+    .spacer {
+      flex: 0;
+    }
+  }
+}
+
+.handle {
+  border-radius: 50%;
+  background: $white;
+  width: 36px;
+  height: 36px;
+}
+
+.spacer {
+  flex: 1;
+  transition: flex $animation linear;
+}
+</style>
--- src/pages/NewTransactionPage.vue
+++ src/pages/NewTransactionPage.vue
@@ -1,24 +1,31 @@
 <script setup lang="ts">
 import VgInput from '@/components/ui/forms/VgInput.vue'
 import VgInputNumber from '@/components/ui/forms/VgInputNumber.vue'
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
 import VgButton from '@/components/ui/VgButton.vue'
+import VgInputCheck from '@/components/ui/forms/VgInputCheck.vue'
 
 const formValues = reactive({
   title: '',
-  amount: 0
+  amount: 0,
+  recurrent: false
+})
+
+const isValid = computed(() => {
+  return formValues.amount > 0
 })
 </script>
 
 <template>
   <div class="page-wrapper">
     <div class="text-title-4">
-      New Expense
+      New Transaction
     </div>
     <div class="form-wrapper">
       <VgInputNumber v-model="formValues.amount" label="Amount" />
       <VgInput v-model="formValues.title" label="Description (Optional)" />
-      <VgButton>Create</VgButton>
+      <VgInputCheck v-model="formValues.recurrent" label="Make recurrent" />
+      <VgButton :disabled="!isValid">Create</VgButton>
     </div>
   </div>
 </template>
