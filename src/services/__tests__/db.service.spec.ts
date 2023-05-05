05fafd440c80e8eb7aee3ff62974e814c6e1feb6
DavidValero <dvdvalero89@gmail.com>
2023-05-05 13:08:23 +0200

    First DB service test

---
+++ src/services/__tests__/db.service.spec.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect, beforeAll, vi } from 'vitest'
+import { fetchMonths, initDB } from '../db.service'
+
+describe('DB Service', () => {
+  beforeAll(async () => {
+    vi.mock('dexie')
+    await initDB()
+  })
+
+  it('clears undefined months on fetchMonths', async () => {
+    const result = await fetchMonths(['2023-05', '2023-04', '2023-03', '2023-02', '2023-01'])
+    expect(result).toEqual([
+      { monthId: '2023-05', income: 123, outcome: 321 },
+      { monthId: '2023-04', income: 456, outcome: 654 }
+    ])
+  })
+})
