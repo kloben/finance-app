7c0d4d7c44e67abd70dc0b70212e478339c69fea
DavidValero <dvdvalero89@gmail.com>
2023-05-05 13:08:22 +0200

    Updated store fetch. Remove predictions

--- src/stores/finances.store.ts
+++ src/stores/finances.store.ts
@@ -7,7 +7,6 @@ import { toMonthId } from '@/helpers/date.helper'
 interface StoreState {
   savings: number | null
   monthIds: string[]
-  prediction: Omit<IMonth, 'monthId'>
   monthsCache: Map<string, IMonth>
   paymentsCache: Map<string, Map<number, IPayment>> // Grouped by month
 }
@@ -16,11 +15,14 @@ export const useFinancesStore = defineStore('finances', {
   state: (): StoreState => ({
     savings: null,
     monthIds: [],
-    prediction: { income: 0, outcome: 0 },
     monthsCache: new Map<string, IMonth>(),
     paymentsCache: new Map<string, Map<number, IPayment>>()
   }),
   getters: {
+    currentMonth: (state: StoreState): IMonth => {
+      const monthId = state.monthIds[state.monthIds.length - 1]
+      return state.monthsCache.get(monthId) ?? { monthId, income: 0, outcome: 0 }
+    },
     lastMonths: (state: StoreState): IMonth[] => {
       return state.monthIds
         .map((monthId) => state.monthsCache.get(monthId) ?? { monthId, income: 0, outcome: 0 })
@@ -33,38 +35,24 @@ export const useFinancesStore = defineStore('finances', {
   },
   actions: {
     async init (): Promise<void> {
-      this.monthIds = getMonthIds()
-      await getMonths(this.monthIds).then(months => {
-        months.forEach((month) => {
-          this.monthsCache.set(month.monthId, month)
-        })
-      })
-      for (const monthId of this.monthIds.slice(-3)) {
-        await fetchPayments(monthId).then(payments => {
-          this.paymentsCache.set(monthId, payments.reduce((map, payment) => {
+      const monthIds = getMonthIds()
+      const months = await getMonths(monthIds)
+      const payments = await getPayments(monthIds)
+      const savings = fetchSavings()
+
+      this.$patch((state: StoreState) => {
+        state.monthIds = monthIds
+        state.savings = savings
+        for (const month of months) {
+          state.monthsCache.set(month.monthId, month)
+        }
+        for (const monthId in payments) {
+          state.paymentsCache.set(monthId, payments[monthId].reduce((map, payment) => {
             map.set(payment.id, payment)
             return map
           }, new Map()))
-        })
-      }
-      this.savings = fetchSavings()
-      this.reCalculatePrediction()
-    },
-    reCalculatePrediction (): void {
-      const totals: { [type in PaymentType]: Record<string, number> } = {
-        income: {},
-        outcome: {}
-      }
-      for (const monthId of this.monthIds.slice(-3)) {
-        const payments = Array.from(this.paymentsCache.get(monthId)?.values() ?? [])
-        for (const payment of payments) {
-          totals[payment.type][payment.category] = (totals[payment.type][payment.category] ?? 0) + payment.amount
         }
-      }
-      this.prediction = {
-        income: Math.round(Object.keys(totals.income).reduce((carry, keyId) => carry + (totals.income[keyId] / 3), 0)),
-        outcome: Math.round(Object.keys(totals.outcome).reduce((carry, keyId) => carry + (totals.outcome[keyId] / 3), 0))
-      }
+      })
     },
     updateSavings (newValue: number): void {
       storeSavings(newValue)
@@ -72,19 +60,18 @@ export const useFinancesStore = defineStore('finances', {
     },
     async createPayment (date: Date, paymentData: IPaymentData) {
       const { payment, month, savings } = await storePayment(date, paymentData)
-      if (savings !== null) {
-        this.savings = savings
-      }
-      if (this.monthsCache.has(month.monthId)) {
-        this.monthsCache.set(month.monthId, month)
-      }
-      const payments = this.paymentsCache.get(payment.monthId)
-      if (payments) {
-        payments.set(payment.id, payment)
-      }
-      if (this.monthIds.includes(payment.monthId)) {
-        this.reCalculatePrediction()
-      }
+      this.$patch((state: StoreState) => {
+        if (savings !== null) {
+          state.savings = savings
+        }
+        if (state.monthsCache.has(month.monthId)) {
+          state.monthsCache.set(month.monthId, month)
+        }
+        const payments = state.paymentsCache.get(payment.monthId)
+        if (payments) {
+          payments.set(payment.id, payment)
+        }
+      })
     }
   }
 })
@@ -103,4 +90,12 @@ async function getMonths (monthIds: string[]): Promise<IMonth[]> {
     .then(months => monthIds.map((monthId: string): IMonth => {
       return months.find(m => m.monthId === monthId) ?? { monthId, income: 0, outcome: 0 }
     }))
+}
+
+async function getPayments (monthIds: string[]): Promise<Record<string, IPayment[]>> {
+  const payments: Record<string, IPayment[]> = {}
+  for (const monthId of monthIds) {
+    payments[monthId] = await fetchPayments(monthId)
+  }
+  return payments
 }
