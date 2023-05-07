5b472d1224b7377e078c8ad87b01b2ece5a39616
DavidValero <dvdvalero89@gmail.com>
2023-05-07 09:37:07 +0200

    Add DB migration to store totals in DB

--- src/models/month.interface.ts
+++ src/models/month.interface.ts
@@ -1,5 +1,10 @@
+export interface IMonthTotals {
+  [category: string]: number
+}
+
 export interface IMonth {
   monthId: string // YYYY-MM
   income: number
   outcome: number
+  totals: IMonthTotals
 }
--- src/services/db.client.ts
+++ src/services/db.client.ts
@@ -1,5 +1,5 @@
 import Dexie, { type Table, type Transaction } from 'dexie'
-import type { IMonth } from '@/models/month.interface'
+import type { IMonth, IMonthTotals } from '@/models/month.interface'
 import type { IPayment } from '@/models/payment.interface'
 import type { ICategory } from '@/models/category.interface'
 import { categoriesV1 } from '@/data/categories'
@@ -23,6 +23,22 @@ class AppDexie extends Dexie {
       [DB_TABLE.months]: '&monthId',
       [DB_TABLE.payments]: '++id, monthId',
       [DB_TABLE.categories]: 'id'
+    })
+
+    this.version(2).stores({
+      [DB_TABLE.months]: '&monthId',
+      [DB_TABLE.payments]: '++id, monthId',
+      [DB_TABLE.categories]: '&id'
+    }).upgrade(async (tx: Transaction) => {
+      const months: IMonth[] = await tx.table(DB_TABLE.months).toArray()
+      for (const { monthId } of months) {
+        const totals: IMonthTotals = {}
+        const payments: IPayment[] = await tx.table(DB_TABLE.payments).where({ monthId }).toArray()
+        for (const payment of payments) {
+          totals[payment.category ?? ''] = (totals[payment.category ?? ''] ?? 0) + payment.amount
+        }
+        await tx.table(DB_TABLE.months).update(monthId, { totals })
+      }
     })
 
     this.on('populate', (tx: Transaction) => {
