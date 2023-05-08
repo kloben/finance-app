9b8eba7d2a5073ffc4b5da94b086c73e47aff098
David Valero <valero@yiqqi.com>
2023-05-08 12:53:35 +0200

    Finished detail store tests

--- src/stores/__tests__/detail.store.spec.ts
+++ src/stores/__tests__/detail.store.spec.ts
@@ -1,6 +1,9 @@
 import { afterAll, beforeAll, beforeEach, describe, vi, it, expect } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia'
 import { useDetailStore } from '../detail.store'
+import {TestMonths} from "../../services/__mocks__/data";
+import {fetchMonths, fetchPayments} from "../../services/__mocks__/db.service";
+import {getEmptyMonth} from "../../helpers/data.helper";
 
 describe('DetailStore', () => {
   beforeAll(() => {
@@ -22,5 +25,31 @@ describe('DetailStore', () => {
     expect(store.monthId).toBe(null)
     expect(store.month).toEqual(null)
     expect(store.payments).toEqual([])
+  })
+
+  it('loads month data', async () => {
+    const store = useDetailStore()
+    await store.loadMonth('2023-04')
+
+    expect(store.monthId).toBe('2023-04')
+    expect(store.month).toEqual(TestMonths['2023-04'])
+    expect(store.payments.length).toBe(2)
+
+    expect(fetchMonths).toHaveBeenCalledTimes(1)
+    expect(fetchMonths).toHaveBeenCalledWith(['2023-04'])
+    expect(fetchPayments).toHaveBeenCalledTimes(1)
+    expect(fetchPayments).toHaveBeenCalledWith('2023-04')
+  })
+
+  it('skip loading month if same as current', async () => {
+    const store = useDetailStore()
+    store.monthId = '2023-04'
+    await store.loadMonth('2023-04')
+
+    expect(store.monthId).toBe('2023-04')
+    expect(store.month).toEqual(getEmptyMonth('2023-04'))
+
+    expect(fetchMonths).toHaveBeenCalledTimes(0)
+    expect(fetchPayments).toHaveBeenCalledTimes(0)
   })
 })
