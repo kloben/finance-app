c39b022ac95d94fd43e3766f6742b61e4833b253
David Valero <valero@yiqqi.com>
2023-05-08 22:09:01 +0200

    WIP making custom footer background

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import VgBarChart from '@/components/ui/charts/VgBarChart.vue'
-import { useGlobalStore } from '@/stores/global.store'
-import { computed, onMounted } from 'vue'
-import type { IMonth } from '@/models/month.interface'
-import { toMonthLabel } from '@/helpers/date.helper'
-import { toCurrency } from '@/helpers/number.helper'
-import { useHomeStore } from '@/stores/home.store'
-import type { BarChartData, PieChartData } from '@/components/ui/charts/chart-value.interface'
+import {useGlobalStore} from '@/stores/global.store'
+import {computed, onMounted} from 'vue'
+import type {IMonth} from '@/models/month.interface'
+import {toMonthLabel} from '@/helpers/date.helper'
+import {toCurrency} from '@/helpers/number.helper'
+import {useHomeStore} from '@/stores/home.store'
+import type {BarChartData, PieChartData} from '@/components/ui/charts/chart-value.interface'
 import VgPieChart from '@/components/ui/charts/VgPieChart.vue'
-import { PaymentType } from '@/models/payment.interface'
+import {PaymentType} from '@/models/payment.interface'
 
 const globalStore = useGlobalStore()
 const store = useHomeStore()
@@ -43,9 +43,15 @@ onMounted(() => {
 
 <template>
   <div class="page-wrapper">
+
+    <div class="footer-container">
+      <div class="footer"></div>
+    </div>
+
+
     <div class="text-title-4">Total savings: {{ savings }}</div>
-    <VgBarChart :data="barData" />
-    <VgPieChart :data="pieData" />
+    <VgBarChart :data="barData"/>
+    <VgPieChart :data="pieData"/>
   </div>
 </template>
 
@@ -55,4 +61,34 @@ onMounted(() => {
 .text-title-4 {
   padding-bottom: 32px;
 }
+
+.footer-container {
+  width: 100%;
+  height: 75px;
+  background: grey;
+
+  $cut: 30px;
+  $round: 20px;
+
+  --bg-layer-1: radial-gradient(
+                  circle at 50% 0,
+                  transparent #{$cut},
+                  #fff calc(#{$cut} + 1px)
+  ) 0 #{$round} / 100% calc(100% - #{$round}) no-repeat no-repeat;
+  --bg-layer-2: radial-gradient(
+                  circle at calc(100% - #{$cut}) 100%,
+                  #fff #{$cut},
+                  transparent calc(#{$cut} + 1px)
+  ) 0 0 / calc(50% - #{$cut}) #{$cut} no-repeat no-repeat;
+
+
+  .footer {
+    width: 100%;
+    height: 100%;
+    background: var(--bg-layer-1), var(--bg-layer-2);
+
+    // x-pos y-pos / width height no-repeat
+  }
+}
+
 </style>
