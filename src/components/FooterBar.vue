44054ad64dd71a5ea1c0cae0de84caa43b8332ba
DavidValero <dvdvalero89@gmail.com>
2023-05-09 17:03:35 +0200

    First version of footer container

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -5,46 +5,64 @@ import VgIconChart from '@/components/ui/icons/VgIconChart.vue'
 </script>
 
 <template>
-  <div class="footer-bar">
-    <router-link to="/detail">
-      <div class="item" data-test-id="detail-link">
-        <VgIconChart />
-      </div>
-    </router-link>
-    <router-link to="/new">
-      <div class="item" data-test-id="new-link">
-        <VgIconNew />
-      </div>
-    </router-link>
-    <router-link to="/predict">
-      <div class="item" data-test-id="prediction-link">
-        <VgIconPrediction />
-      </div>
-    </router-link>
+  <div class="footer-container">
   </div>
+<!--  <div class="footer-bar">-->
+<!--    <router-link to="/detail">-->
+<!--      <div class="item" data-test-id="detail-link">-->
+<!--        <VgIconChart />-->
+<!--      </div>-->
+<!--    </router-link>-->
+<!--    <router-link to="/new">-->
+<!--      <div class="item" data-test-id="new-link">-->
+<!--        <VgIconNew />-->
+<!--      </div>-->
+<!--    </router-link>-->
+<!--    <router-link to="/predict">-->
+<!--      <div class="item" data-test-id="prediction-link">-->
+<!--        <VgIconPrediction />-->
+<!--      </div>-->
+<!--    </router-link>-->
+<!--  </div>-->
 </template>
 
 <style scoped lang="scss">
 @import "src/styles/colors";
 
-.footer-bar {
+$big: 41px;
+$small: 33px;
+$smallOffset: 26px;
+
+
+.footer-container {
+  width: 100%;
+  height: 82px;
   position: fixed;
   bottom: 0;
   left: 0;
-  background: $white;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 8px;
-}
-
-.item {
-  cursor: pointer;
-  padding: 12px;
-
-  &:hover {
-    background: darken($white, 10);
-  }
+  filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.1));
+  background:
+  radial-gradient(
+                  circle at 50% calc($big / 2 * -1),
+                  transparent #{$big},
+                  #fff calc(#{$big} + 1px)
+  ) 0 calc($big / 2) / 100% calc(100% - #{$big} / 2) no-repeat no-repeat,
+  radial-gradient(
+                  circle at calc(50% - #{$big} - #{$smallOffset}) #{$small},
+                  #fff #{$small},
+                  transparent calc(#{$small} + 1px)
+  ) 0 0 / 100% 100% no-repeat no-repeat,
+  radial-gradient(
+                  circle at calc(50% + #{$big} + #{$smallOffset}) #{$small},
+                  #fff #{$small},
+                  transparent calc(#{$small} + 1px)
+  ) 0 0 / 100% 100% no-repeat no-repeat,
+  linear-gradient(
+                  90deg,
+                  #fff calc(50% - #{$big} - #{$smallOffset}),
+                  transparent calc(50% - #{$big} - #{$smallOffset} + 1px),
+                  transparent calc(50% + #{$big} + #{$smallOffset} - 1px),
+                  #fff calc(50% + #{$big} + #{$smallOffset})
+  ) 0 0 / 100% 100% no-repeat no-repeat;
 }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -43,15 +43,6 @@ onMounted(() => {
 
 <template>
   <div class="page-wrapper">
-
-    <div class="footer-container">
-      <div class="guide big"></div>
-      <div class="guide small a"></div>
-      <div class="guide small b"></div>
-      <div class="footer"></div>
-    </div>
-
-
     <div class="text-title-4">Total savings: {{ savings }}</div>
     <VgBarChart :data="barData" />
     <VgPieChart :data="pieData" />
@@ -64,78 +55,4 @@ onMounted(() => {
 .text-title-4 {
   padding-bottom: 32px;
 }
-
-.footer-container {
-  width: 100%;
-  height: 82px;
-  background: grey;
-  position: relative;
-
-  $big: 41px;
-  $small: 33px;
-  $smallOffset: 25px;
-
-  --bg-layer-1: radial-gradient(
-                  circle at 50% 0,
-                  transparent #{$big},
-                  #fff calc(#{$big} + 1px)
-  ) 0 #{$small} / 100% calc(100% - #{$small}) no-repeat no-repeat;
-  --bg-layer-2: radial-gradient(
-                  circle at calc(100% - #{$big}) 100%,
-                  #fff #{$big},
-                  transparent calc(#{$big} + 1px)
-  ) 0 0 / calc(50% - #{$big}) #{$big} no-repeat no-repeat;
-  --bg-layer-3: radial-gradient(
-                  circle at #{$big} 100%,
-                  #fff #{$big},
-                  transparent calc(#{$big} + 1px)
-  ) 100% 0 / calc(50% - #{$big}) #{$big} no-repeat no-repeat;
-  --bg-layer-4: linear-gradient(
-                  90deg,
-                  #fff calc(50% - #{$big} - #{$small}),
-                  transparent calc(50% - #{$big} - #{$small} + 1px),
-                  transparent calc(50% + #{$big} + #{$small} - 1px),
-                  #fff calc(50% + #{$big} + #{$small})
-  ) 0 0 / 100% #{$small} no-repeat no-repeat;
-  --bg-layer-test: linear-gradient(
-                  90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%
-  ) 100% 0 / calc(50% - #{$big}) #{$big} no-repeat no-repeat;
-  //
-  // x-pos y-pos / width height no-repeat
-
-  .footer {
-    width: 100%;
-    height: 100%;
-    background: var(--bg-layer-1), var(--bg-layer-2), var(--bg-layer-3), var(--bg-layer-4)
-  }
-
-  .guide {
-    border-radius: 50%;
-    aspect-ratio: 1;
-    opacity: 0.5;
-    position: absolute;
-
-    &.big {
-      background: #02c902;
-      height: calc($big * 2);
-      top: -50%;
-      left: calc(50% - $big);
-    }
-
-    &.small {
-      background: #c9023e;
-      height: calc($small * 2);
-      top: 0;
-
-      &.a {
-        left: calc(50% - $big - $small - $smallOffset);
-      }
-
-      &.b {
-        left: calc(50% + $big - $small + $smallOffset);
-      }
-    }
-  }
-}
-
 </style>
