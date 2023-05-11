fc0be2e2f15f3b819d0052d396e498f1de9577f9
David Valero <valero@yiqqi.com>
2023-05-11 19:22:38 +0200

    Add basic FuturePage spec

---
+++ src/views/__tests__/FuturePage.spec.ts
@@ -0,0 +1,38 @@
+import { describe, it, afterAll, beforeAll, beforeEach, vi, expect } from 'vitest';
+import { createPinia, setActivePinia } from 'pinia';
+import { flushPromises, mount, VueWrapper } from '@vue/test-utils';
+import { useGlobalStore } from '../../stores/global.store';
+import { TestPayments } from "../../services/__mocks__/test-data";
+import FuturePage from "../FuturePage.vue";
+
+function generateWrapper (): VueWrapper<FuturePage> {
+  return mount(FuturePage, {
+    global: {
+      stubs: ['VgBarChart', 'VgPieChart']
+    }
+  })
+}
+
+describe('FuturePage', () => {
+  beforeAll(() => {
+    vi.useFakeTimers()
+    vi.setSystemTime(new Date(2023, 3, 30))
+    vi.mock('../../services/db.service.ts')
+  })
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+  afterAll(() => {
+    vi.useRealTimers()
+  })
+
+  it('Mounts with current data', async () => {
+    await useGlobalStore().init()
+    const wrapper = generateWrapper()
+    await flushPromises()
+
+    expect(wrapper.vm.chartData.length).toBe(5)
+    expect(wrapper.vm.chartData[0].predicted).toBe(false)
+    expect(wrapper.vm.chartData[1].predicted).toBe(true)
+  })
+})
